8a988f806b4ec82ed40646290bcfb03bd6958332
ipeefreely <mayaju@gmail.com>
2015-03-12 19:00:19 +1100

    Changes made by Ihab!

--- ENetCare.UnitTest/PackageServiceUnitTest.cs
+++ ENetCare.UnitTest/PackageServiceUnitTest.cs
@@ -71,5 +71,33 @@ namespace ENetCare.UnitTest
 
             Assert.AreNotEqual<string>(string.Empty, barCode);
         }
+
+        //ADDED BY IHAB, JUST A TEST CHANGE TO SEE IF GIT IS WORKING OR NOT
+        [TestMethod]
+        public void TestRegisterPackage2()
+        {
+            IPackageRepository packageRepository = new MockPackageRepository();
+            PackageService packageService = new PackageService(packageRepository);
+            StandardPackageType packageType = new StandardPackageType
+            {
+                PackageTypeId = 1,
+                Description = "100 Panadol Pills",
+                ShelfLifeUnitType = ShelfLifeUnitType.Day,
+                ShelfLifeUnits = 45
+            };
+
+            DistributionCentre location = new DistributionCentre
+            {
+                CentreId = 2,
+                Name = "North Centre",
+                Address = "Up the river",
+                IsHeadOffice = false
+            };
+
+            string barCode;
+            int packageId = packageService.Register(packageType, location, DateTime.Today.AddMonths(2), out barCode);
+
+            Assert.AreNotEqual<string>(string.Empty, barCode);
+        }
     }
 }
