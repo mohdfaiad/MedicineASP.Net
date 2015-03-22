ae26f4aea38d02b11237f65d5ce49ca18f46e702
mrcactus <cactus0@yahoo.com>
2015-03-23 04:35:41 +1100

    -

--- ENetCare.Repository/Repository/IEmployeeRepository.cs
+++ ENetCare.Repository/Repository/IEmployeeRepository.cs
@@ -12,7 +12,10 @@ namespace ENetCare.Repository.Repository
         void Update(Employee employee);
 
         Employee Get(int? employeeId, string username);
-
-        List<DistributionCentre> GetAllDistributionCentres();
+        List<DistributionCentre> GetAllDistributionCentres();        // * * * *
+        List<Employee> GetAllEmployees();
+        Employee getEmployeeById(int id);
+        List<Employee> getEmployeesAtCentre(DistributionCentre xCentre);
+        List<Employee> getEmployeesByType(EmployeeType xType);
     }
 }
