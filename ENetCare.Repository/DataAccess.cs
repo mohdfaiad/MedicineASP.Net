4c5d825204c4f100564857d77940d75297b414fd
benrogers1967 <benrogers1967@yahoo.com.au>
2015-03-23 22:33:09 +1100

    Revert "getAllPackages added"
    This reverts commit ec9f1ae87f64f8062794d168a856666b9183edf3.

--- ENetCare.Repository/DataAccess.cs
+++ ENetCare.Repository/DataAccess.cs
@@ -158,16 +158,16 @@ namespace ENetCare.Repository
 
 
 
-        public static List<Package> GetAllPackages(SqlConnection connection)
+        public static Package GetAllPackages(SqlConnection connection)
         {                                                          // Added by Pablo on 23-03-15
             Package package = null;
             string query = "SELECT PackageId, BarCode, ExpirationDate, PackageTypeId, CurrentLocationCentreId, CurrentStatus, DistributedByEmployeeId FROM Package ";
             var cmd = new SqlCommand(query);
-            List<Package> allPackages = new List<Package>();
             cmd.Connection = connection;
+            cmd.Parameters.AddWithValue("@barcode", string.IsNullOrEmpty(barcode) ? (object)DBNull.Value : barcode);
             using (SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default))
             {
-                while (reader.Read())
+                if (reader.Read())
                 {
                     package = new Package();
                     package.PackageType = new StandardPackageType();
@@ -186,10 +186,9 @@ namespace ENetCare.Repository
                         package.DistributedBy = new Employee();
                         package.DistributedBy.EmployeeId = Convert.ToInt32(reader["DistributedByEmployeeId"]);
                     }
-                    allPackages.Add(package);
                 }
             }
-            return allPackages;
+            return package;
         }
 
 
