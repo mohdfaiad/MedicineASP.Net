177f22be724fe73be02f17295c6dfd6f07b434b7
mrcactus <cactus0@yahoo.com>
2015-03-24 15:51:43 +1100

    PackageTransit class

---
+++ ENetCare.Repository/Data/PackageTransit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENetCare.Repository.Data
+{
+    class PackageTransit
+    {
+        public int TransitId { get; set; }
+        public Package Package { get; set; }
+        public DistributionCentre SenderCentre { get; set; }
+        public DistributionCentre ReceiverCentre { get; set; }
+        public DateTime DateSent { get; set; }
+        public DateTime DateReceived { get; set; }
+        public DateTime DateCancelled { get; set; }
+
+    }
+}
