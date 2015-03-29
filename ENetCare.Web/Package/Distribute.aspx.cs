57e4bae32bab36692fbfbc0097dbf9fdc8d69c0b
ipeefreely <mayaju@gmail.com>
2015-03-29 16:28:04 +0100

    Fully working Distribution Page

--- ENetCare.Web/Package/Distribute.aspx.cs
+++ ENetCare.Web/Package/Distribute.aspx.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using ENetCare.BusinessService;
+using ENetCare.Repository.Data;
+using ENetCare.Repository.Repository;
+using ENetCare.Web.Membership;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -10,33 +15,62 @@ namespace ENetCare.Web
 {
     public partial class Distribute : System.Web.UI.Page
     {
+        private PackageService _packageService;
         protected void Page_Load(object sender, EventArgs e)
         {
+            IPackageRepository packageRepository = new PackageRepository(ConfigurationManager.ConnectionStrings["ENetCare"].ConnectionString);
+            _packageService = new PackageService(packageRepository);
 
+            if (!Page.IsPostBack)
+            {
+                IEmployeeRepository repository = new EmployeeRepository(ConfigurationManager.ConnectionStrings["ENetCare"].ConnectionString);
+                var employeeService = new EmployeeService(repository);
+
+                EmployeeMembershipUser user = (EmployeeMembershipUser)System.Web.Security.Membership.GetUser();
+                DistributionCentre centre = employeeService.GetDistributionCentre(user.DistributionCentreId);              
+
+                ViewState["DistributionCentre"] = centre;
+                ViewState["EmployeeUsername"] = user.UserName;
+            }
         }
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
+            DistributionCentre centre = (DistributionCentre)ViewState["DistributionCentre"];
+
+            IEmployeeRepository repository = new EmployeeRepository(ConfigurationManager.ConnectionStrings["ENetCare"].ConnectionString);
+            var employeeService = new EmployeeService(repository);
+
+            string employeeUsername = (string)ViewState["EmployeeUsername"];
+
+            Employee employee = employeeService.Retrieve(employeeUsername);
+
+            DateTime expirationDate = DateTime.Now;            
+
             List<string> barcodes = ucPackageBarcode.GetBarcodes();
-            StringBuilder barcodeListText = new StringBuilder();
             for (int i = 0; i < barcodes.Count(); i++)
             {
-                if (i > 0)
-                    barcodeListText.Append(", ");
+                string packageTypeId = ucPackageBarcode.GetPackageTypeId(barcodes[i]);
 
-                barcodeListText.Append(barcodes[i]);
+                Package package = _packageService.Retrieve(barcodes[i]);
+
+                StandardPackageType spt = _packageService.GetStandardPackageType(package.PackageType.PackageTypeId);
+                
+                _packageService.Distribute(barcodes[i], centre, employee, expirationDate, spt, package.PackageId);
             }
 
             Page.ClientScript.RegisterStartupScript(
-                Page.GetType(),
+                Page.GetType(),  
                 "MessageBox",
-                "<script language='javascript'>alert('" + barcodeListText.ToString() + "');</script>"
+                "<script language='javascript'>alert('Packages have been successfully Distributed');</script>"
             );
+
+            Response.Redirect("Distribute.aspx");
         }
 
         protected void btnCancel_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Distribute.aspx");
         }
     }
 }
