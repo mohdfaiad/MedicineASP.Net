13ca207ace51526dd6e577b7c434781f3ab5cadc
mrcactus <cactus0@yahoo.com>
2015-03-31 14:22:42 +1100

    "TransitResult" created

---
+++ ENetCare.BusinessService/TransitResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENetCare.BusinessService
+{
+    class TransitResult
+    {
+
+        public const string BarCodeNotFound = "Bar Code not found";
+        public const string PackageElsewhere = "Package appears as located elsewhere";
+        public const string PackageNotInStock = "Package appears not to be in Stock";
+        public const string PackageAlreadyAtDestination = "Package appears as being already at the Destination Centre";
+        public const string TransitNotFound = "Transit not found";
+        public const string MoreThanOneTransitForPackage = "More than one active transit exists for that package";
+    
+    }
+}
