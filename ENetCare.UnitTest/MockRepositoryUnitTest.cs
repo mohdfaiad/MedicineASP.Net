801f14866490f1d7cccf51db5b42af70fd123ae2
mrcactus <cactus0@yahoo.com>
2015-04-06 03:18:09 +1000

    "MockRepositoryUnitTest" added.   This is just for me to have access to the state of the mock repository at any given time. Pablo

---
+++ ENetCare.UnitTest/MockRepositoryUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ENetCare.Repository;
+using ENetCare.Repository.Data;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace ENetCare.UnitTest
+{
+    [TestClass]
+    public class MockRepositoryUnitTest
+    {
+
+
+        [TestMethod]
+        public void TestMockTables()
+        {
+            MockDataAccess.LoadMockTables();
+
+            List<DistributionCentre> distList = MockDataAccess.GetAllDistibutionCentres();
+            Debug.WriteLine("DISTRIBUTION CENTRES : ");
+            foreach (DistributionCentre centre in distList) Debug.WriteLine(centre);
+
+            List<Employee> employeeList = MockDataAccess.GetAllEmployees();
+            Debug.WriteLine("\n\n EMPLOYEES : ");
+            foreach (Employee emp in employeeList) Debug.WriteLine(emp);
+
+
+            List<StandardPackageType> typeList = MockDataAccess.GetAllPackageTypes();
+            Debug.WriteLine("\n\n STANDARD PACKAGE TYPES : ");
+            foreach (StandardPackageType t in typeList) Debug.WriteLine(t);
+
+
+            List<Package> packageList = MockDataAccess.GetAllPackages();
+            Debug.WriteLine("\n\n PACKAGES : ");
+            foreach (Package p in packageList) Debug.WriteLine(p);
+
+
+            List<PackageTransit> transitList = MockDataAccess.GetAllPackageTransits();
+            Debug.WriteLine("\n\n PACKAGE TRANSITS : ");
+            foreach (PackageTransit t in transitList) Debug.WriteLine(t);
+
+            Assert.IsNotNull(employeeList);
+        }
+
+
+
+
+    }
+}
