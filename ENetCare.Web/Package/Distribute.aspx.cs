d5d45af317a56c260c123d8c41a74efc7d45f2ce
ipeefreely <mayaju@gmail.com>
2015-04-06 12:41:27 +0100

    Compile Include PackageResult.cs

--- ENetCare.Web/Package/Distribute.aspx.cs
+++ ENetCare.Web/Package/Distribute.aspx.cs
@@ -36,6 +36,13 @@ namespace ENetCare.Web
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
+            if (!Page.IsValid)
+            {
+                pnlErrorMessage.Visible = true;
+                litErrorMessage.Text = "There are errors";
+                return;
+            }
+
             DistributionCentre centre = (DistributionCentre)ViewState["DistributionCentre"];
 
             IEmployeeRepository repository = new EmployeeRepository(ConfigurationManager.ConnectionStrings["ENetCare"].ConnectionString);
@@ -55,15 +62,21 @@ namespace ENetCare.Web
                 Package package = _packageService.Retrieve(barcodes[i]);
 
                 StandardPackageType spt = _packageService.GetStandardPackageType(package.PackageType.PackageTypeId);
-                
-                _packageService.Distribute(barcodes[i], centre, employee, expirationDate, spt, package.PackageId);
-            }
 
-            Page.ClientScript.RegisterStartupScript(
-                Page.GetType(),  
-                "MessageBox",
-                "<script language='javascript'>alert('Packages have been successfully Distributed');</script>"
-            );
+                var result = _packageService.Distribute(barcodes[i], centre, employee, expirationDate, spt, package.PackageId);
+                if (!result.Success)
+                {
+                    var err = new CustomValidator();
+                    err.ValidationGroup = "destinationDetails";
+                    err.IsValid = false;
+                    err.ErrorMessage = result.ErrorMessage;
+                    Page.Validators.Add(err);
+
+                    pnlErrorMessage.Visible = true;
+                    litErrorMessage.Text = "There are errors";
+                    return;
+                }
+            }
 
             Response.Redirect("Distribute.aspx");
         }
