370ad4728fa0d1a7e026ac1ecb7d7486c5af004b
ipeefreely <mayaju@gmail.com>
2015-04-06 15:03:47 +0100

    Added 2 new messages

--- ENetCare.BusinessService/PackageResult.cs
+++ ENetCare.BusinessService/PackageResult.cs
@@ -12,5 +12,7 @@ namespace ENetCare.BusinessService
         public const string PackageElsewhere = "Package is NOT located in this distribution centre: ";
         public const string PackageAlreadyDistributed = "Package has been already distributed: ";
         public const string PackageInTransit = "The following package is in Transit: ";
+        public const string EmployeeNotAuthorized = "You are not authorized to distribute packages";
+        public const string EmployeeInDifferentLocation = "The following package is not in the current distribution centre: ";
     }
 }
