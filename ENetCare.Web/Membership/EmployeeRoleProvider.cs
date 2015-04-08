30ac536312014d52db3944b1ff85eabdceded3c0
benrogers1967 <benrogers1967@yahoo.com.au>
2015-04-08 21:18:06 +1000

    Restrict access to web pages by roles

---
+++ ENetCare.Web/Membership/EmployeeRoleProvider.cs
@@ -0,0 +1,92 @@
+﻿using ENetCare.BusinessService;
+using ENetCare.Repository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace ENetCare.Web.Membership
+{
+    public class EmployeeRoleProvider : RoleProvider
+    {
+        private EmployeeService _employeeService;
+
+        public EmployeeRoleProvider()
+        {
+            IEmployeeRepository repository = new EmployeeRepository(ConfigurationManager.ConnectionStrings["ENetCare"].ConnectionString);
+            _employeeService = new EmployeeService(repository);            
+        }        
+        
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            var employee = _employeeService.Retrieve(username);
+            if (employee == null)
+                return new string[] { };
+            else
+                return new string[] { employee.EmployeeType.ToString() };
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            var employee = _employeeService.Retrieve(username);
+            if (employee != null && employee.EmployeeType.ToString() == roleName)
+                return true;
+
+            return false;
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
