2585c5813fe173eaf9372751868a03dcd55b68bf
mrcactus <cactus0@yahoo.com>
2015-04-16 04:36:52 +1000

    stocktaking report

---
+++ ENetCare.Web/Report/Stocktacking.aspx.cs
@@ -0,0 +1,41 @@
+﻿using ENetCare.BusinessService;
+using ENetCare.Repository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ENetCare.Web.Report
+{
+    public partial class Stocktaking : System.Web.UI.Page
+    {
+        private decimal _grandTotalValue = 0;
+        private ReportService _reportService;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IReportRepository repository = new ReportRepository(ConfigurationManager.ConnectionStrings["ENetCare"].ConnectionString);
+            _reportService = new ReportService(repository);
+
+            grd.DataSource = _reportService.GetDoctorActivity();
+            grd.DataBind();
+        }
+
+        protected void grd_RowCreated(object sender, GridViewRowEventArgs e)
+        {
+        }
+
+        protected void grd_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+           
+        }
+
+        protected void btnClose_OnClick(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Home.aspx");
+        }
+    }
+}
